f5631f52d447533319462a662180fffd6a44d5fc
isha-web1 <isha.web1@gmail.com>
2025-02-02 15:27:19 +0600

    eslint setup

--- dist/config/index.js
+++ dist/config/index.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-exports.default = app;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.default = {
+    port: process.env.PORT,
+    db_url: process.env.DB_URL
+};
